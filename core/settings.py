3749dcb9c9c8d213e090430ea9060d3b583d830d
Eren <rahulsaarika@email.com>
2026-04-14 17:28:22 +0530

    ready for deploy

--- core/settings.py
+++ core/settings.py
@@ -14,6 +14,7 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 import os
 import ssl
+import dj_database_url
 
 load_dotenv()
 from pathlib import Path
@@ -82,14 +83,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'daylog'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
 
 
